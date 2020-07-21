aa7e93c70e73db9b5e1d484c28d39cb321b39f98
Amir Fazleev <doaxan77@gmail.com>
2020-07-21 21:52:41 +0300

    Add react-bootstrap:Container, Navbar, Nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {Container, Navbar, Nav} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'Amir Fazleev',
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'},
+      ],
+      home: {
+        title: 'Welcome!',
+        subTitle: 'Subtitle',
+        text: 'Checkout my projects below'
+      },
+      About: {
+        title: 'About Me',
+      },
+      contact: {
+        title: 'Let\'s talk!',
+      },
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Amir Fazleev</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+        </Container>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
