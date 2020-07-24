7194d736f3991ad615f851063bc727430858be5f
Amir Fazleev <doaxan77@gmail.com>
2020-07-24 23:53:24 +0300

    Change email to hello@AmirFazleev.ru

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -8,7 +8,7 @@ function ContactPage(props) {
       <Hero title={props.title}/>
       <Content>
         <h5 className="font-weight-light">
-          <p>Email: <a href="mailto:hello@doaxan.me">hello@doaxan.me</a></p>
+          <p>Email: <a href="mailto:hello@AmirFazleev.ru">hello@AmirFazleev.ru</a></p>
           <p>Telegram: <a href="https://t.me/doaxan">@doaxan</a></p>
         </h5>
       </Content>
